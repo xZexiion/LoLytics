777897b7e68c1e6c21ea56a2fec52f144a58e211
Fahd <fahd31256@gmail.com>
2025-07-18 19:09:17 +0100

    Add website title

--- lolytics/src/App.jsx
+++ lolytics/src/App.jsx
@@ -1,7 +1,12 @@
+import './App.css';
+
 function App() {
     return (
-        <h1>Hello, world!</h1>
-    )
+        <div className="container">
+            <div className="title">LoLytics</div>
+        </div>
+    );
 }
+
 
 export default App
