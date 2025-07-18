cde54d07548598fe68061318a93d77db5a91372d
Fahd <fahd31256@gmail.com>
2025-07-18 19:27:28 +0100

    feat: landing page input bar

--- lolytics/src/App.jsx
+++ lolytics/src/App.jsx
@@ -4,6 +4,10 @@ function App() {
     return (
         <div className="container">
             <div className="title">LoLytics</div>
+            <div className="input-group">
+                <input className="input-left" placeholder="Name" />
+                <input className="input-right" placeholder="Tag" />
+            </div>
         </div>
     );
 }
