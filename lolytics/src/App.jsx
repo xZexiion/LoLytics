80dc2a24ef20652acf078995ab861344d59d4039
Fahd <fahd31256@gmail.com>
2025-07-23 17:41:46 +0100

    style(site): animate search bar and title upon pressing search button

--- lolytics/src/App.jsx
+++ lolytics/src/App.jsx
@@ -1,17 +1,27 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+    const [searched, setSearched] = useState(false);
+
+    const handleSearch = () => {
+        setSearched(true);
+    };
+
     return (
         <div className="container">
-            <div className="title">LoLytics</div>
-            <div className="input-group">
-                <input className="input-left" placeholder="Name" />
-                <input className="input-right" placeholder="Tag" />
+            <div className={`title ${searched ? 'title-shrink' : ''}`}>LoLytics</div>
+            <div className={`input-group-wrapper ${searched ? 'fade-out' : ''}`}>
+                <div className="input-group">
+                    <input className="input-left" placeholder="Name" />
+                    <input className="input-right" placeholder="Tag" />
+                </div>
+                <button className="search-button" onClick={handleSearch}>
+                    <span className="search-button-text">Search</span>
+                </button>
             </div>
-            <button className="search-button"><span className='search-button-text'>Search</span></button>
         </div>
     );
 }
 
-
-export default App
+export default App;
