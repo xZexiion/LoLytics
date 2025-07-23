3916262b1c54ae738664a34ea7ae3eaf76028a0b
Fahd <fahd31256@gmail.com>
2025-07-23 18:50:19 +0100

    feat: simple http dev server with autoreloading

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
-console.log('Starting the server...');
+import '../data/game.js';
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/match_history/', (req, res) => {
+    const { name, tag } = req.query;
+    res.send(`Fetching match history for ${name}#${tag}`)
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
