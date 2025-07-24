fix(model): normalize features using z-score normalization

model/dnn.py
@@ -15,24 +15,30 @@ class DNN(nn.Module):
             nn.Linear(128, 1)
         )
 
+        self.kill_indices = torch.tensor([0, 11, 22, 33, 44, 96, 107, 118, 129, 140])
+        self.death_indices = torch.tensor([1, 12, 23, 34, 45, 97, 108, 119, 130, 141])
+        self.assist_indices = torch.tensor([2, 13, 24, 35, 46, 98, 109, 120, 131, 142])
+        self.gold_indices = torch.tensor([6, 17, 28, 39, 50, 102, 113, 124, 135, 146])
+        self.cs_indices = torch.tensor([8, 19, 30, 41, 52, 104, 115, 126, 137, 148])
+        self.baron_indices = torch.tensor([3, 14, 25, 36, 47, 99, 110, 121, 132, 143])
+        self.elder_indices = torch.tensor([4, 15, 26, 37, 48, 100, 111, 122, 133, 144])
+        self.death_timer_indices = torch.tensor([5, 16, 27, 38, 49, 101, 112, 123, 134, 145])
+        self.level_indices = torch.tensor([7, 18, 29, 40, 51, 103, 114, 125, 136, 147])
+        self.x_indices = torch.tensor([9, 20, 31, 42, 53, 105, 116, 127, 138, 149])
+        self.y_indices = torch.tensor([10, 21, 32, 43, 54, 106, 117, 128, 139, 150])
+
     def normalize(x):
-        for team_offset in [0, 11*5+24+3+11+3]:
-            for j in range(5):
-                player_offset = j * 11
-                offset = team_offset + player_offset
-
-                x[:, offset] = (x[:, offset] - 2.686) / 3.281
-                x[:, offset+1] = (x[:, offset+1] - 2.698) / 2.754
-                x[:, offset+2] = (x[:, offset+2] - 3.259) / 4.216
-                x[:, offset+6] = (x[:, offset+6] - 5840.304) / 4262.559
-                x[:, offset+8] = (x[:, offset+8] - 88.101) / 74.53
-
-                x[:, offset+3] /= 3*60
-                x[:, offset+4] /= 2*60+30
-                x[:, offset+5] /= 79
-                x[:, offset+7] /= 18
-                x[:, offset+9] /= 14500
-                x[:, offset+10] /= 14500
+        x[:, self.kill_indices] = (x[:, self.kill_indices] - 2.686) / 3.281
+        x[:, self.death_indices] = (x[:, self.death_indices] - 2.698) / 2.754
+        x[:, self.assist_indices] = (x[:, self.assist_indices] - 3.259) / 4.216
+        x[:, self.gold_indices] = (x[:, self.gold_indices] - 5840.304) / 4262.559
+        x[:, self.cs_indices] = (x[:, self.cs_indices] - 88.101) / 74.53
+        x[:, self.baron_indices] /= 3*60
+        x[:, self.elder_indices] /= 2*60+30
+        x[:, self.death_timer] /= 79
+        x[:, self.level_indices] /= 18
+        x[:, self.x_indices] /= 14500
+        x[:, self.y_indices] /= 14500
         
     def forward(self, x):
         B = x.size(0)