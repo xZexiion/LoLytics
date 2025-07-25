7151836bf4e731e31b215c7cdd2ef94dbca3f36d
Fahd <fahd31256@gmail.com>
2025-07-25 13:34:39 +0100

    fix(dataset): update Dataset class to load entire lmdb file

--- model/dataset.py
+++ model/dataset.py
@@ -2,31 +2,15 @@ import torch
 import torch.utils.data as dutils
 import pickle
 import lmdb
-import random
-
-def load_data(data_path):
-    random.seed(42)
-    keys = []
-    env = lmdb.open('dataset.lmdb', readonly=True, lock=False)
-    with env.begin() as txn:
-        with txn.cursor() as cursor:
-            for key, _ in cursor:
-                keys.append(key)
-    random.shuffle(keys)
-
-    return {
-        'env': env,
-        'keys': keys
-    }
 
 class Dataset(dutils.Dataset):
-    def __init__(self, keys, env, training, split):
-        self.env = env
-        num_train_samples = int(len(keys) * split)
-        if training:
-            self.keys = keys[:num_train_samples]
-        else:
-            self.keys = keys[num_train_samples:]
+    def __init__(self, data_path):
+        self.keys = []
+        self.env = lmdb.open(data_path, readonly=True, lock=False)
+        with self.env.begin() as txn:
+            with txn.cursor() as cursor:
+                for key, _ in cursor:
+                    self.keys.append(key)
 
     def __len__(self):
         return len(self.keys)
@@ -40,7 +24,7 @@ class Dataset(dutils.Dataset):
         return sample[:-1], sample[-1]
 
 if __name__ == '__main__':
-    ds = Dataset('dataset.lmdb', False, 0.8)
+    ds = Dataset('dataset.lmdb')
     print(len(ds))
     a = next(iter(ds))
     print(a)
