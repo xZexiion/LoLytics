691f0cae25a1091439204ee8760fa0faec563eb1
Fahd <fahd31256@gmail.com>
2025-07-25 14:17:31 +0100

    fix(metrics): proper weight device mapping when loading the model state dict

--- model/calculate_accuracy.py
+++ model/calculate_accuracy.py
@@ -1,11 +1,10 @@
 import torch
-from dataset import Dataset, load_data
+from dataset import Dataset
 from dnn import DNN
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 
-data = load_data('dataset.lmdb')
-dataset = Dataset(data['keys'], data['env'], False, 0.8)
+dataset = Dataset('test.lmdb')
 
 device = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
 print(f'Using {device}')
@@ -17,8 +16,8 @@ t = [0] * 40
 
 
 net = DNN()
-net.load_state_dict(torch.load('net.pth', weights_only=True))
-net = net.to(device)
+net.load_state_dict(torch.load('net.pth', map_location=device, weights_only=True))
+# net = net.to(device)
 
 with torch.no_grad():
     for _ in tqdm(range(100000)):
@@ -28,10 +27,10 @@ with torch.no_grad():
         time = inputs[-1]
         if time >= 40:
             continue
-        inputs = torch.tensor(inputs).view(1, -1).to(device)
+        inputs = torch.tensor(inputs).view(1, -1)
         y = torch.sigmoid(net(inputs)).round()
 
-        l[time] += (y == torch.tensor(label).to(device)).float().item()
+        l[time] += (y == torch.tensor(label, dtype=torch.float)).item()
         t[time] += 1
 
 for i in range(40):
@@ -39,6 +38,9 @@ for i in range(40):
 
 x = list(range(40))
 
-plt.figure(figsize=(8, 8))
+plt.figure(figsize=(5, 5))
 plt.bar(x, l)
+plt.xlabel('Time Interval')
+plt.ylabel('Accuracy')
+plt.title('Model Accuracy Over Time')
 plt.show()
